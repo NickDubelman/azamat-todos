bbc3c728e946b7c735254ba2da0306a507b3f014
Nick Dubelman <ndubelman@gmail.com>
2022-03-20 18:07:40 -0700

    leverage ID that is returned by InsertBuilder.Run

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ var TodoTable = azamat.Table[Todo]{
 }
 
 func main() {
+	// Connect to db
 	db, err := sqlx.Connect("sqlite3", ":memory:")
 	if err != nil {
 		panic(err)
@@ -36,11 +37,10 @@ func main() {
 	// Insert
 	todoTitle := "assist Borat"
 	insert := TodoTable.Insert().Columns("title").Values(todoTitle)
-	result, err := insert.Run(db)
+	todoID, err := insert.Run(db)
 	if err != nil {
 		panic(err)
 	}
-	todoID, _ := result.LastInsertId()
 	fmt.Printf("Inserted todo: id=%d, title=%s\n", todoID, todoTitle)
 
 	// Query all
@@ -51,32 +51,32 @@ func main() {
 	fmt.Println("Query all:", todos)
 
 	// Query by ID
-	todo, err := TodoTable.GetByID(db, int(todoID))
+	todo, err := TodoTable.GetByID(db, todoID)
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println("Query by ID:", todo)
 
 	todoTitle = "no longer friends with borat"
-	update := TodoTable.Update().Set("title", todoTitle).Where("id = ?", todoID)
+	update := TodoTable.
+		Update().
+		Set("title", todoTitle).
+		Where("id = ?", todoID)
+
 	if _, err := update.Run(db); err != nil {
 		panic(err)
 	}
-
 	fmt.Printf("Updated todo: id=%d, title=%s\n", todoID, todoTitle)
 
-	todo, err = TodoTable.GetByID(db, int(todoID))
+	todo, err = TodoTable.GetByID(db, todoID)
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("Query by ID:", todos[0])
+	fmt.Println("Query by ID:", todo)
 
 	delete := TodoTable.Delete().Where("id = ?", todoID)
 	if _, err := delete.Run(db); err != nil {
 		panic(err)
 	}
-
 	fmt.Printf("Deleted todo: id=%d\n", todoID)
 }
